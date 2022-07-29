52aee3e720685cba1b051d3278fd0fb4daa16c9f
ale lord <j.alexlord@gmail.com>
2022-07-29 16:14:10 -0500

    query params and fetch room info

--- frontend/pages/room/index.js
+++ frontend/pages/room/index.js
@@ -1,19 +1,8 @@
-import { useState } from "react"
-import Link from "next/link"
-
 export default function room() {
-    const [votesToSkip, setVotesToSkip] = useState(2)
-    const [guestCanPause, setGuestCanPause] = useState(false)
-    const [isHost, setIsHost] = useState(false)
 
   return (
     <>
-        <p>{votesToSkip}</p>
-        <p>{guestCanPause}</p>
-        <p>{isHost}</p>
-        <Link href={'/room/'}>
-          <a></a>
-        </Link>
+        <h1>Room</h1>
     </>
   )
 }

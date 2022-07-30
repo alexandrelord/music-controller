0eaba5d8a85dda08ee0e176f689099830628ae80
ale lord <j.alexlord@gmail.com>
2022-07-30 11:23:26 -0500

    integrate django-debug-toolbar to project

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -48,10 +48,11 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'corsheaders',
-
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -60,6 +61,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'config.urls'

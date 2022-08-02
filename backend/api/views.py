5966aa392f13333ec61948fe1415369eb0550f41
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-08-02 17:05:03 -0500

    add LeaveRoom view and url pattern

--- backend/api/views.py
+++ backend/api/views.py
@@ -56,11 +56,11 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
-                self.request.session['room_code'] = room.code
+                # self.request.session['room_code'] = room.code
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
-                self.request.session['room_code'] = room.code
+                # self.request.session['room_code'] = room.code
             
             return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
@@ -77,9 +77,20 @@ class JoinRoomView(APIView):
             room_result = Room.objects.filter(code=code)
             if len(room_result) > 0:
                 room = room_result[0]
-                self.request.session['room_code'] = code
+                # self.request.session['room_code'] = code
                 return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
             
             return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
         
         return Response({'Bad Request': 'Invalid post data, did not find a code key!'}, status=status.HTTP_400_BAD_REQUEST)
+
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        host_id = self.request.session.session_key
+        room_results = Room.objects.filter(host=host_id)
+        if len(room_results) > 0:
+            room = room_results[0]
+            room.delete()
+
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
+        

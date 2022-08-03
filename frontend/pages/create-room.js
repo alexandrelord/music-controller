13222c9673188d766f218554a28c3deff628743c
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-08-03 15:33:06 -0500

    add create and update form component - refactor create page render function

--- frontend/pages/create-room.js
+++ frontend/pages/create-room.js
@@ -1,17 +1,7 @@
 import { useState } from "react"
 import { useRouter } from "next/router"
 import axios from "../src/utils/axios"
-import Link from "next/link"
-
-import Button from "@mui/material/Button"
-import Grid from "@mui/material/Grid"
-import Typography from "@mui/material/Typography"
-import TextField from "@mui/material/TextField"
-import FormHelperText from "@mui/material/FormHelperText"
-import FormControl from "@mui/material/FormControl"
-import Radio from "@mui/material/Radio"
-import RadioGroup from "@mui/material/RadioGroup"
-import FormControlLabel from "@mui/material/FormControlLabel"
+import CreateAndUpdateForm from "../components/CreateAndUpdateForm"
 
 function CreateRoom() {
   const defaultVotes = 2
@@ -38,74 +28,14 @@ function CreateRoom() {
     .catch(error => console.error(error))
   }
 
-    return (
-      <Grid container spacing={1}>
-        <Grid item xs={12} align="center">
-          <Typography component="h4" variant="h4">
-            Create a Room
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <FormControl component="fieldset">
-            <FormHelperText sx={{ textAlign: "center" }}>
-                Guest Control of Playback State
-            </FormHelperText>
-            <RadioGroup row defaultValue="true" onChange={handleGuestCanPauseChange}>
-              <FormControlLabel 
-                value="true" 
-                  control={<Radio color="primary" />} 
-                  label="Play/Pause"
-                  labelPlacement="bottom"
-              />
-              <FormControlLabel 
-                value="false" 
-                  control={<Radio color="secondary" />} 
-                  label="No Control"
-                  labelPlacement="bottom"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <FormControl>
-            <TextField 
-              required={true} 
-              type="number" 
-              defaultValue={ defaultVotes }
-               inputProps={{
-                  min: 1,
-                  style: { textAlign: "center" }
-              }} 
-              onChange={handleVotesChange}
-            />
-            <FormHelperText sx={{ textAlign: "center" }}>
-              Votes Required to Skip Song
-            </FormHelperText>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Button 
-            color="primary" 
-            variant="contained"
-            disableRipple 
-            onClick={handleRoomButtonClick}
-          >
-            Create A Room
-          </Button>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Link href="/" passHref>  
-            <Button 
-            color="secondary" 
-            variant="contained" 
-            disableRipple
-            >
-              Back
-            </Button>
-          </Link>
-        </Grid>
-      </Grid>
-    )
-  }
+  return (
+    <CreateAndUpdateForm 
+    defaultVotes={defaultVotes}
+    handleGuestCanPauseChange={handleGuestCanPauseChange}
+    handleVotesChange={handleVotesChange}
+    handleRoomButtonClick={handleRoomButtonClick}
+    />
+  )
+}
   
   export default CreateRoom
